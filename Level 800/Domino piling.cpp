745dace1e06bb3052d2d1d184910c796d64b21f9
Huỳnh Viết Tuấn Kiệt <84212036+HiImKing1509@users.noreply.github.com>
2021-10-08 10:43:30 +0700

    Problem in Codeforces

---
+++ Level 800/Domino piling.cpp
@@ -0,0 +1,28 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <numeric>
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+	int a, b;
+	cin >> a >> b;
+	cout << a * b / 2;
+	return 0;
+};
