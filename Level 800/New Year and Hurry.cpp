5352c812eb4d6674b739c297ce1d650f96e2b0c3
Huỳnh Viết Tuấn Kiệt <84212036+HiImKing1509@users.noreply.github.com>
2021-10-11 11:34:04 +0700

    Problem in Codeforces

--- Level 800/New Year and Hurry.cpp
+++ Level 800/New Year and Hurry.cpp
@@ -1,11 +1,11 @@
-                /*------------------------------------------------------------------------*            
-                |    User: Huynh Viet Tuan Kiet                                           |
-                |    Degree: Student - Formal University                                  |
-                |    Study Computer Science at University of Information Technology       |
-                |    Country: Viet Nam                                                    |
-                |    City: Ho Chi Minh                                                    |
-                |    Organization: VNUHCM - UIT - University of Information Technology    | 
-                *-------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
 #include <iostream>
 #include <vector>
 #include <queue>
@@ -18,34 +18,34 @@ using namespace std;
 typedef long long ll;
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0); cout.tie(0);
-    
-    int n, k;
-    cin >> n >> k;
-    int tgk = 240 - k;
-    int tong = n * (n + 1) / 2;
-    int sum = 0;
-    int vti = 0;
-    if (5 * tong <= tgk)
-        cout << n;
-    else
-    {
-        for (int i = 1; i <= n; i++)
-        {
-            sum = sum + i * 5;
-            if (sum > tgk)
+            ios_base::sync_with_stdio(false);
+            cin.tie(0); cout.tie(0);
+
+            int n, k;
+            cin >> n >> k;
+            int tgk = 240 - k;
+            int tong = n * (n + 1) / 2;
+            int sum = 0;
+            int vti = 0;
+            if (5 * tong <= tgk)
+                   cout << n;
+            else
             {
-                vti = i - 1;
-                i = n + 1;
+                   for (int i = 1; i <= n; i++)
+                   {
+                          sum = sum + i * 5;
+                          if (sum > tgk)
+                          {
+                                  vti = i - 1;
+                                  i = n + 1;
+                          }
+                          else if (sum == tgk)
+                          {
+                                  vti = i;
+                                  i = n + 1;
+                          }
+                   }
+                   cout << vti;
             }
-            else if (sum == tgk)
-            {
-                vti = i;
-                i = n + 1;
-            }
-        }
-        cout << vti;
-    }
-    return 0;
+            return 0;
 }
