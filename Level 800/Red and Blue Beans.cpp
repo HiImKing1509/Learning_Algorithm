8625df65c8931d0cd3e21e0fe7aa2c04af53a78b
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-14 10:28:33 +0700

    Problem in Codeforces

---
+++ Level 800/Red and Blue Beans.cpp
@@ -0,0 +1,63 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000000
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+ll GCD(ll a, ll b) {
+	while (b) { a = a % b; swap(a, b); }
+	return a;
+} // Find GCD of 2 numbers
+ll findGCD(vector<ll>& arr) {
+	ll n = arr.size(); ll result = arr[0];
+	for (ll i = 1; i < n; i++) { result = GCD(arr[i], result); if (result == 1) return 1; }
+	return result;
+} // Find GCD of n numbers (n > 2)
+
+void SolveProblem()
+{
+	ll r, b, d;
+	cin >> r >> b >> d;
+	if ((++d) * min(r, b) >= max(r, b)) cout << "YES";
+	else cout << "NO";
+	cout << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
