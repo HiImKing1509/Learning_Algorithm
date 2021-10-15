c27b1c6132d59cb0fecb9ad4185d39d3743eac88
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-15 17:09:08 +0700

    Problem in Codeforces

---
+++ Level 800/Elections.cpp
@@ -0,0 +1,58 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000007
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+void SolveProblem()
+{
+	ll a, b, c;
+	cin >> a >> b >> c;
+	if (max(b, c) + 1 - a <= 0) cout << 0 << " ";
+	else cout << max(b, c) + 1 - a << " ";
+	if (max(a, c) + 1 - b <= 0) cout << 0 << " ";
+	else cout << max(a, c) + 1 - b << " ";
+	if (max(a, b) + 1 - c <= 0) cout << 0;
+	else cout << max(b, a) + 1 - c;
+	cout << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
