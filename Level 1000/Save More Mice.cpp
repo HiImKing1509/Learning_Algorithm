2ba0da08b704a681488de6ec64d1693206bede8c
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-15 17:13:52 +0700

    Problem in Codeforces

---
+++ Level 1000/Save More Mice.cpp
@@ -0,0 +1,68 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000000
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+void SolveProblem()
+{
+	ll n, k, sum = 0, c = 0;
+	cin >> n >> k;
+	vector<ll>a(k);
+	vector<ll>res;
+	for (ll i = 0; i < k; i++)
+	{
+		cin >> a[i];
+		res.push_back(n - a[i]);
+	}
+	sort(res);
+	for (ll i = 0; i < res.size(); i++)
+	{
+		if (sum + res[i] <= n - 1)
+		{
+			sum += res[i];
+			c++;
+		}
+	}
+	cout << c << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
