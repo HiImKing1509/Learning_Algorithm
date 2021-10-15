cdf11efea90238b3b63b81ca2dff08598cbbea59
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-15 17:26:12 +0700

    Problem in Codeforces

---
+++ Level 800/Maximum GCD.cpp
@@ -0,0 +1,40 @@
+﻿/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+#define MAX 26
+#define MIN -1
+#define ll long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int th;
+	cin >> th;
+	while (th--)
+	{
+		ll n;
+		cin >> n;
+		if (n % 2 == 0)
+			cout << n / 2;
+		else
+			cout << (n - 1) / 2;
+		cout << "\n";
+	}
+	return 0;
+}
