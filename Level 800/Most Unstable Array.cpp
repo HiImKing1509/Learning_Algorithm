d45c7e0cdc9b5ec7986b280932bf011848ec00fb
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-17 13:03:12 +0700

    Problem in Codeforces

---
+++ Level 800/Most Unstable Array.cpp
@@ -0,0 +1,42 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+#define MAX 100000
+#define MIN -100000
+#define ll long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	ll th;
+	cin >> th;
+	while (th--)
+	{
+		ll n, m;
+		cin >> n >> m;
+		if (n == 1)
+			cout << 0;
+		else if (n == 2)
+			cout << m;
+		else
+			cout << 2 * m;
+		cout << "\n";
+	}
+	return 0;
+}
