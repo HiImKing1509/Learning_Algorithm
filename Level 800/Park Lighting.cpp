280d70be14d68fd673f39750570fd3443b4320b1
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-18 00:39:55 +0700

    Problem in Codeforces

---
+++ Level 800/Park Lighting.cpp
@@ -0,0 +1,39 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <cmath>
+#include <set>
+using namespace std;
+
+#define MAX 100000
+#define MIN -100000
+#define ll long long
+
+void SolveProblem()
+{
+	int n, m;
+	cin >> n >> m;
+	cout << (n * m + 1) / 2 << "\n";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int th;
+	cin >> th;
+	while (th--)
+		SolveProblem();
+	return 0;
+}
