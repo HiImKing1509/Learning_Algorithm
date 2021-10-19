c34b17cfa81c4929d85a177ade8316d0dc1eaa7d
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-20 00:41:17 +0700

    Problem in Codeforces

---
+++ Level 800/CopyCopyCopyCopyCopyc.cpp
@@ -0,0 +1,49 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define MAX 1000000000
+#define MIN -100000
+#define ll long long
+
+
+void SolveProblem()
+{
+	ll n;
+	cin >> n;
+	vector<ll>a(n);
+	set<ll>se;
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		se.insert(a[i]);
+	}
+	cout << se.size() << "\n";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	ll th;
+	cin >> th;
+	while (th--)
+		SolveProblem();
+	return 0;
+}
