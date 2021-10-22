4beb0d3d307c2b102cc0454d99ed64c75acad8a1
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-22 18:29:45 +0700

    Problem in Codeforces

---
+++ Level 800/Gregor and Cryptography.cpp
@@ -0,0 +1,36 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+#define MAX 100000
+#define MIN -100000
+#define ll long long
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	ll th;
+	cin >> th;
+	while (th--)
+	{
+		ll n;
+		cin >> n;
+		cout << 2 << " " << n - 1 << "\n";
+	}
+	return 0;
+}
