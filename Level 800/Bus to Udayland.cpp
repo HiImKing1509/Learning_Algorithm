fbb2edb9ab568a81bc04df9b3cb3541bd3acac29
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-22 18:09:19 +0700

    Problem in Codeforces

---
+++ Level 800/Bus to Udayland.cpp
@@ -0,0 +1,77 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000007
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+void SolveProblem()
+{
+	ll n;
+	cin >> n;
+	vector<string>a(n);
+	bool flag = false;
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (!flag)
+		{
+			if (a[i][0] == 'O' && a[i][1] == 'O')
+			{
+				flag = true;
+				a[i][0] = a[i][1] = '+';
+			}
+			if (!flag && a[i][3] == 'O' && a[i][4] == 'O')
+			{
+				flag = true;
+				a[i][3] = a[i][4] = '+';
+			}
+		}
+	}
+	if (!flag)
+		cout << "NO";
+	else
+	{
+		cout << "YES\n";
+		for (ll i = 0; i < n; i++)
+			cout << a[i] << "\n";
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//_TestCases_
+		SolveProblem();
+	return 0;
+}
