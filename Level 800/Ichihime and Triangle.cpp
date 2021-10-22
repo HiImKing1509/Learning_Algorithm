67bdeb6b97c17040f9bc670a92e68f1f11dff988
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-22 17:39:47 +0700

    Problem in Codeforces

---
+++ Level 800/Ichihime and Triangle.cpp
@@ -0,0 +1,51 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000000
+#define MIN -1
+
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == long long
+
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned long long
+
+
+void SolveProblem()
+{
+	ll a, b, c, d;
+	cin >> a >> b >> c >> d;
+	cout << b << " " << c << " " << c << "\n";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
+ 
