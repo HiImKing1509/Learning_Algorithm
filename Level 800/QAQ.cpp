d9b80eecc4623fcaca0f80039edcfbeae2f0ee1e
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-22 17:28:07 +0700

    Problem in Codeforces

---
+++ Level 800/QAQ.cpp
@@ -0,0 +1,76 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000000
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+ll GCD(ll a, ll b) {
+	while (b) { a = a % b; swap(a, b); }
+	return a;
+} // Find GCD of 2 numbers
+ll LCM(ll a, ll b) {
+	return (a * b) / GCD(a, b);
+} // Find LCM of 2 numbers 
+ll findGCD(vector<ll>& arr) {
+	ll n = arr.size(); ll result = arr[0];
+	for (ll i = 1; i < n; i++) { result = GCD(arr[i], result); if (result == 1) return 1; }
+	return result;
+} // Find GCD of n numbers (n > 2) --- O(n)
+
+void SolveProblem()
+{
+	ll res = 0;
+	string s;
+	cin >> s;
+
+	if (s.size() <= 2) res = 0;
+	else
+	{
+		for (ll i = 0; i < s.size() - 2; i++)
+			for (ll j = i + 1; j < s.size(); j++)
+				for (ll k = j + 1; k < s.size(); k++)
+					if (s[i] == 'Q' and s[j] == 'A' and s[k] == 'Q')
+						res++;
+	}
+	cout << res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//_TestCases_
+		SolveProblem();
+	return 0;
+}
