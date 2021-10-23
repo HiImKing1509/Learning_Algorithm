e0e8eba9155e90f36184114960c6c533e5cc5767
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-23 07:49:20 +0700

    Problem in Codeforces

---
+++ Level 900/New Year's Number.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+typedef long long ll;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	ll th;
+	cin >> th;
+
+	queue<string>kq;
+	for (ll k = 0; k < th; k++)
+	{
+		ll n;
+		cin >> n;
+		if (n <= 2019)
+			kq.push("NO");
+		else
+		{
+			if (n % 2020 == 0 || n % 2021 == 0)
+				kq.push("YES");
+			else
+			{
+				ll mod = n / 2020;
+				ll t = n - 2020 * mod;
+				if (t <= mod || t == 2021)
+					kq.push("YES");
+				else
+					kq.push("NO");
+			}
+		}
+	}
+	for (ll i = kq.size(); i > 0; i--)
+	{
+		cout << kq.front() << "\n";
+		kq.pop();
+	}
+	return 0;
+}
