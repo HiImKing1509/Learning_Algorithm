d1a1cb87ff390cf1a2c7db67bfde499bf11c8d2e
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-30 10:41:35 +0700

    Problem in Codeforces

---
+++ Level 1100/Similar Pairs.cpp
@@ -0,0 +1,70 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000007
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+
+void SolveProblem()
+{
+	ll n, e = 0, o = 0;
+	cin >> n;
+	vector<ll>a(n);
+	for (ll i = 0; i < n; i++)
+	{
+		cin >> a[i];
+		if (a[i] % 2 == 0) e++;
+		else o++;
+	}
+	if (e % 2 == 0) cout << "YES";
+	else
+	{
+		sort(a);
+		bool flag = false;
+		for (ll i = 1; i < n; i++)
+			if (a[i] - a[i - 1] == 1)
+				flag = true;
+		if (flag) cout << "YES";
+		else cout << "NO";
+	}
+	cout << "\n";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
