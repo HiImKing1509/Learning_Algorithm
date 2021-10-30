4a281e2f4f019f5d5a113fa1408d914c871f615d
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-30 10:40:36 +0700

    Problem in Codeforces

---
+++ Level 1100/Double Cola.cpp
@@ -0,0 +1,59 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000007
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+
+void SolveProblem()
+{
+    ll n, i = 0;
+	cin >> n;
+    while (n > 5 * ceil(pow(2, i)))
+        n -= 5 * ceil(pow(2, i++));
+    ll ans = (n - 1) / ceil(pow(2, i));
+    if (ans == 0) cout << "Sheldon";
+    else if (ans == 1) cout << "Leonard";
+    else if (ans == 2) cout << "Penny";
+    else if (ans == 3) cout << "Rajesh";
+    else if (ans == 4) cout << "Howard";
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//_TestCases_
+		SolveProblem();
+	return 0;
+}
