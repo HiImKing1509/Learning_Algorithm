d99c979577a58f746d61e3a3f9ff79c21d67be7b
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-30 11:29:13 +0700

    Problem in Codeforces

---
+++ Level 1400/Books.cpp
@@ -0,0 +1,75 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 101
+#define MIN -1
+
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+
+#define pq priority_queue
+
+void SolveProblem()
+{
+	ll n, t, sum = 0, m = 0;
+	cin >> n >> t;
+	vector<ll>a(n);
+	queue<ll>que;
+	for (ll i = 0; i < n; i++)
+		cin >> a[i];
+	for (ll i = 0; i < n; i++)
+	{
+		sum += a[i];
+		que.push(a[i]);
+		if (sum > t)
+		{
+			ll t = que.size() - 1;
+			m = max(m, t);
+			sum -= que.front();
+			que.pop();
+		}
+		if (i == n - 1)
+		{
+			if (sum <= t)
+			{
+				ll t = que.size();
+				m = max(m, t);
+			}
+		}
+	}
+	cout << m;
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//_TestCases_
+		SolveProblem();
+	return 0;
+}
