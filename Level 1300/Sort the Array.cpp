b6181e2bb9bab11853598378cc19dd61abbd50ba
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-30 11:09:36 +0700

    Problem in Codeforces

---
+++ Level 1300/Sort the Array.cpp
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000007
+#define MIN -1
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sort(v) sort(v.begin(), v.end())
+#define _sort(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+void SolveProblem()
+{
+	ll n, vtmin = -1, vtmax = -1;
+	cin >> n;
+	vector<ll>a(n);
+	for (ll i = 0; i < n; i++)
+		cin >> a[i];
+	vector<ll>b(a.begin(), a.end());
+	sort(b);
+	for (ll i = 0; i < n; i++)
+	{
+		if (a[i] != b[i])
+		{
+			vtmin = i;
+			break;
+		}
+	}
+	for (ll i = n - 1; i >= 0; i--)
+	{
+		if (a[i] != b[i])
+		{
+			vtmax = i;
+			break;
+		}
+	}
+	if (vtmin == -1 && vtmax == -1)
+		cout << "yes\n" << 1 << " " << 1;
+	else
+	{
+		ll m = vtmax;
+		bool f = true;
+		for (ll i = vtmin; i <= vtmax; i++)
+		{
+			if (a[i] != b[m])
+				f = false;
+			m--;
+		}
+		if (f) cout << "yes\n" << vtmin + 1 << " " << vtmax + 1;
+		else cout << "no";
+	}
+}
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	//_TestCases_
+		SolveProblem();
+	return 0;
+}
