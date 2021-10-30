456d8f43d0e62be8ee39131f4d648bd4d38d912a
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-10-30 10:55:51 +0700

    Problem in Codeforces

---
+++ Level 1200/K-th Not Divisible by n.cpp
@@ -0,0 +1,56 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at University of Information Technology       |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define MAX 1000000000
+#define MIN -1
+
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+
+#define pq priority_queue
+
+void SolveProblem()
+{
+	ll n, k, div, mod, ans;
+	cin >> n >> k;
+	div = k / (n - 1);
+	mod = k % (n - 1);
+	ans = n * div + mod;
+	if (mod == 0)
+		ans--;
+	cout << ans << '\n';
+};
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+	SolveProblem();
+	return 0;
+}
