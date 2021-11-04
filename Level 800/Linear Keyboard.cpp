8af4704a4818f3ed23521c105440a852c2be6af1
Huynh Viet Tuan Kiet <84212036+HiImKing1509@users.noreply.github.com>
2021-11-04 19:19:22 +0700

    Problem in Codeforces

---
+++ Level 800/Linear Keyboard.cpp
@@ -0,0 +1,86 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+#include <iostream>
+#include <climits>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <queue>
+#include <cmath>
+#include <set>
+#include <list>
+#include <map>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pcl pair<char, ll>
+
+ll GCD(ll a, ll b) {
+	while (b) { a = a % b; swap(a, b); }
+	return a;
+} // Find GCD of 2 numbers
+ll LCM(ll a, ll b) {
+	return (a * b) / GCD(a, b);
+} // Find LCM of 2 numbers 
+ll findGCD(vector<ll>& arr) {
+	ll n = arr.size(); ll result = arr[0];
+	for (ll i = 1; i < n; i++) { result = GCD(arr[i], result); if (result == 1) return 1; }
+	return result;
+} // Find GCD of n numbers (n > 2) --- O(n)
+
+void SolveProblem()
+{
+	vector<char>s(26);
+	string str;
+	for (ll i = 0; i < 26; i++) cin >> s[i];
+	cin >> str;
+	if (str.size() == 1) cout << 0;
+	else
+	{
+		ll vt = -1;
+		for (ll j = 0; j < 26; j++)
+			if (str[0] == s[j])
+				vt = j;
+		ll res = 0;
+		for (ll i = 1; i < str.length(); i++)
+		{
+			for (ll j = 0; j < 26; j++)
+			{
+				if (str[i] == s[j])
+				{
+					res += abs(vt - j);
+					vt = j;
+				}
+			}
+		}
+		cout << res;
+	}
+	cout << "\n";
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	_TestCases_
+		SolveProblem();
+	return 0;
+}
