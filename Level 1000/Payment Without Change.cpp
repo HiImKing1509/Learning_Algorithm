d87ea8d74f5b6ad382a81d049ec0992c31e511e2
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-04-27 11:09:16 +0700

    Problem in Codeforces

---
+++ Level 1000/Payment Without Change.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+ll BinarySearch(vector<ll>&arr, ll xx)
+{
+    ll left = 0;
+    ll right = arr.size() - 1;
+    while (left <= right)
+    {
+        ll mid = (left + right) / 2;
+
+        if (arr[mid] == xx)
+            return mid;
+        if (arr[mid] > xx)
+            right = mid - 1;
+        if (arr[mid] < xx)
+            left = mid + 1;
+    }
+    return -1;
+}
+
+void SolveProblem()
+{
+    ll a, b, n, S;
+    cin >> a >> b >> n >> S;
+    if (S - min(a, (S / n)) * n <= b) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    _TestCases_
+        SolveProblem();
+
+    return 0;
+}
+
+// Shortcut key for comment in Visual Studio 2019: Ctrl + K + C
+// Shortcut key for uncomment in Visual Studio 2019: Ctrl + K + U
