a453ea78be5020992cd59dd4bf6408e7bf3d42dd
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-04-28 22:44:35 +0700

    Problem in Codeforces

---
+++ Level 800/Average Height.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+ll BinarySearch(vector<ll>&arr, ll xx)
+{
+    ll left = 0;
+    ll right = arr.size() - 1;
+    while (left <= right)
+    {
+        ll mid = (left + right) / 2;
+
+        if (arr[mid] == xx)
+            return mid;
+        if (arr[mid] > xx)
+            right = mid - 1;
+        if (arr[mid] < xx)
+            left = mid + 1;
+    }
+    return -1;
+}
+
+void SolveProblem()
+{
+    ll n; cin >> n;
+    vector<ll>a(n);
+    deque<ll>d;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        if (a[i] % 2) d.push_back(a[i]);
+        else d.push_front(a[i]);
+    }
+    for (ll i = d.size(); i > 0; i--)
+    {
+        cout << d.back() << " ";
+        d.pop_back();
+    }
+    cout << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    _TestCases_
+        SolveProblem();
+
+    return 0;
+}
+
+// Shortcut key for comment in Visual Studio 2019: Ctrl + K + C
+// Shortcut key for uncomment in Visual Studio 2019: Ctrl + K + U
