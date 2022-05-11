bb10d83a50e5572169674cf7d0734804f8c1cc8a
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-05-11 22:11:30 +0700

    Problem in Codeforces
    https://codeforces.com/contest/1676/problem/A

---
+++ Level 800/Lucky?.cpp
@@ -0,0 +1,74 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+
+ll BinarySearch(vector<ll>&arr, ll xx)
+{
+    ll left = 0;
+    ll right = arr.size() - 1;
+    while (left <= right)
+    {
+        ll mid = (left + right) / 2;
+
+        if (arr[mid] == xx)
+            return mid;
+        if (arr[mid] > xx)
+            right = mid - 1;
+        if (arr[mid] < xx)
+            left = mid + 1;
+    }
+    return -1;
+}
+ll GCD(ll a, ll b)
+{
+    while (b)
+    {
+        a = a % b;
+        swap(a, b);
+    }
+    return a;
+}
+
+void SolveProblem()
+{
+    string s; cin >> s;
+    if ((s[0] - '0') + (s[1] - '0') + (s[2] - '0') == (s[3] - '0') + (s[4] - '0') + (s[5] - '0'))
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    _TestCases_
+        SolveProblem();
+
+    return 0;
+}
+
+// Shortcut key for comment in Visual Studio 2019: Ctrl + K + C
+// Shortcut key for uncomment in Visual Studio 2019: Ctrl + K + U
