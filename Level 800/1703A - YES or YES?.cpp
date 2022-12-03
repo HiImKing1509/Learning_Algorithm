7d54d4c85e426cb19b4210b2b645eb9d80e6239c
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2022-12-03 09:22:25 +0700

    Problem in Codeforces

---
+++ Level 800/1703A - YES or YES?.cpp
@@ -0,0 +1,47 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+//#include <vector>
+//#include <iostream>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+#define psl pair<string, ll>
+#define MOD 1e9 + 7
+
+
+void solve()
+{
+    string s; cin >> s;
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    if (s == "yes") cout << "YES\n";
+    else cout << "NO\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    _TestCases_
+        solve();
+
+    return 0;
+}
