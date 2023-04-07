6ae756adbd942c28dcf90659e975a335a0bdb18d
Huynh Viet Tuan Kiet <hvtkiet.15092002@gmail.com>
2023-04-07 10:55:38 +0700

    Problem in Codeforces

---
+++ Level 800/Insert Digit.cpp
@@ -0,0 +1,64 @@
+/*------------------------------------------------------------------------*
+|    User: Huynh Viet Tuan Kiet                                           |
+|    Degree: Student - Formal University                                  |
+|    Study Computer Science at the University of Information Technology   |
+|    Country: Viet Nam                                                    |
+|    City: Ho Chi Minh                                                    |
+|    Organization: VNUHCM - UIT - University of Information Technology    |
+*-------------------------------------------------------------------------*/
+
+#include <bits/stdc++.h>
+//#include <vector>
+//#include <iostream>
+
+using namespace std;
+
+#define _TestCases_ ll tcs; cin >> tcs; while (tcs--)
+#define db double
+#define ld long double
+#define ll long long
+#define i64 __int64 // == ll
+#define ull unsigned long long
+#define ui64 unsigned __int64 // == unsigned ll
+#define pq priority_queue
+#define sortU(v) sort(v.begin(), v.end())
+#define sortD(v) sort(v.rbegin(), v.rend())
+#define pll pair<ll, ll>
+#define psl pair<string, ll>
+#define MOD 1e9 + 7
+
+
+void solve()
+{
+    ll n;
+    string a;
+    char b;
+    cin >> n >> b;
+    cin >> a;
+    string res = "";
+    for (ll i = 0; i < n; i++)
+    {
+        if (b <= a[i])
+            res += a[i];
+        else
+        {
+            res += b;
+            for (ll j = i; j < n; j++)
+                res += a[j];
+            break;
+        }
+    }
+    if (res.size() == n)
+        res = a + b;
+    cout << res << "\n";
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+    _TestCases_
+        solve();
+
+    return 0;
+}
